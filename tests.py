68a2fda666e3f835e34c6e0a641d6eeaf0bb3f7d
aprilNH7 <79026288+aprilNH7@users.noreply.github.com>
2022-04-04 12:49:12 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -111,20 +111,20 @@ class AgencyTestCase(unittest.TestCase):
         self.assertTrue(data['success'])
 
     def test_delete_actor(self):
-        res = self.client().delete('/actor/60', headers=director_auth)
+        res = self.client().delete('/actor/67', headers=director_auth)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['success'])
-        self.assertEqual(data['deleted'], 60)
+        self.assertEqual(data['deleted'], 67)
 
     def test_delete_movie(self):
-        res = self.client().delete('/movie/60', headers=producer_auth)
+        res = self.client().delete('/movie/67', headers=producer_auth)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['success'])
-        self.assertEqual(data['deleted'], 60)
+        self.assertEqual(data['deleted'], 67)
 
     def test_add_actor_auth_failure(self):
 
