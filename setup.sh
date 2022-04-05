d39e4a5db502375f7b8a9d9d7b96fd1858a2c9f9
aprilNH7 <79026288+aprilNH7@users.noreply.github.com>
2022-04-04 21:19:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+export DATABASE_URL="postgres://gydwbzynakjqou:a1026e14a0c2707fe107c4c63b196bbcb94c8a7a3ea7b9177efcbb620a5b2d6c@ec2-34-224-226-38.compute-1.amazonaws.com:5432/d9a0qbova5m821"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
+export AUTH0_DOMAIN = 'anh-7.us.auth0.com'
+export ALGORITHMS = ['RS256']
+export API_AUDIENCE = 'Agency'
